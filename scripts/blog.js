cd583bc9d64591382ce2d7033a401aeb5cb8a2fc
Paul Schorfheide <pchorf2@gmail.com>
2012-04-12 21:09:38 -0400

    added blog.html

---
+++ scripts/blog.js
@@ -0,0 +1,32 @@
+var url = 'http://annesamuelson.wordpress.com/feed/';
+
+google.load('feeds', '1');
+
+var getEntries = function(url, callback) {
+  var feed = new google.feeds.Feed(url);
+  feed.load(function(result) {
+    callback(result.feed.entries);
+  });
+};
+
+var makeElement = function(entry) {
+  var title = document.createElement('h2');
+  title.innerHTML = entry.title;
+  var blog = document.createElement('div');
+  blog.innerHTML = entry.content;
+  var container = document.createElement('div');
+  container.appendChild(title);
+  container.appendChild(blog);
+  return container;
+};
+
+var insertEntries = function(parent, entries) {
+  var elements = jQuery.map(entries, makeElement);
+  jQuery.map(elements, function(element) { parent.appendChild(element); });
+};
+
+$(document).ready(function() {
+  var entries = getEntries(url, function(entries) { 
+    insertEntries(document.getElementById('blog'), entries);  
+  });
+});
