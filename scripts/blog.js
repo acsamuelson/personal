5f3b5ee7e6dbb9cb1520c6b347b02213abe2b047
Paul Schorfheide <pchorf2@gmail.com>
2012-04-24 21:51:03 -0400

    resizing images

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,4 +1,6 @@
 var url = 'http://annesamuelson.wordpress.com/feed/';
+var MAX_HEIGHT = 340;
+var MAX_WIDTH = 575;
 
 google.load('feeds', '1');
 
@@ -13,6 +15,39 @@ var formatDate = function(date) {
   return $.datepicker.formatDate('MM dd, yy', new Date(date));
 }
 
+var scaleImage = function(image, maxHeight, maxWidth) {
+  removeImageSize(image);
+  $(image).load(function() {
+    var image = $(this);
+    var ratio = 0;
+    var height = this.naturalHeight;
+    var width = this.naturalWidth;
+    if (width > maxWidth) {
+      ratio = maxWidth / width;
+      width = maxWidth;
+      image.css('width', maxWidth);
+      height = height * ratio;
+      image.css('height', height);
+    }
+    if (height > maxHeight) {
+      ratio = maxHeight / height;
+      height = maxHeight;
+      image.css('height', maxHeight);
+      width = width * ratio;
+      image.css('width', width);
+    }
+  });
+}
+
+var removeImageSize = function(elem) {
+  var src = elem.src
+  src = src.replace(/w=[^&]+&?/, '');
+  src = src.replace(/h=[^&]+&?/, '');
+  src = src.replace(/\?$/, '');
+  elem.src = src;
+}
+
+
 var makeElement = function(entry) {
   var title = $(document.createElement('h1'));
   title.append(entry.title);
@@ -23,6 +58,9 @@ var makeElement = function(entry) {
   var blog = $(document.createElement('div'));
   blog.append(entry.content);
   $('a[rel=nofollow]', blog).css('display', 'none');
+  $.map($('img', blog), function(image) {
+    scaleImage(image, MAX_HEIGHT, MAX_WIDTH);
+  });
   $('iframe', blog).remove();
   var container = $(document.createElement('div'));
   container.addClass('blogEntry');
